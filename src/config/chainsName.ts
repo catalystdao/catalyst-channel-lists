933528d0089e4399631002af23fd4b0aa30509f2
Alexander <git@lindgren.xyz>
2024-05-17 13:19:26 +0200

    feat: add blast

--- src/config/chainsName.ts
+++ src/config/chainsName.ts
@@ -6,10 +6,12 @@ export const chainsName = {
   "sepolia": "11155111",
   "arbitrumsepolia": "421614",
   "optimismsepolia": "11155420",
-  "basesepolia": "84532"
+  "basesepolia": "84532",
+  "blasttestnet": "23888"
 };
 
 export const reverseChainsName = {
+  "23888": "blasttestnet",
   "84532": "basesepolia",
   "421614": "arbitrumsepolia",
   "11155111": "sepolia",
