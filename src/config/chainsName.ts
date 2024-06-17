5509740849e0e0f0897eb6e80fba1f33ee201c46
Alexander <git@lindgren.xyz>
2024-06-17 13:07:47 +0200

    fix: change sei testnet to devnet

--- src/config/chainsName.ts
+++ src/config/chainsName.ts
@@ -8,7 +8,7 @@ export const chainsName: Record<string, string> = {
   "optimismsepolia": "11155420",
   "basesepolia": "84532",
   "blasttestnet": "168587773",
-  "seitestnet": "1328",
+  "seidevnet": "713715",
   "xaitestnet": "37714555429",
   "optimism": "10",
   "base": "8453",
@@ -19,13 +19,13 @@ export const chainsName: Record<string, string> = {
 
 export const reverseChainsName: Record<string, string> = {
   "10": "optimism",
-  "1328": "seitestnet",
   "1329": "sei",
   "8453": "base",
   "81457": "blast",
   "84532": "basesepolia",
   "421614": "arbitrumsepolia",
   "660279": "xai",
+  "713715": "seidevnet",
   "11155111": "sepolia",
   "11155420": "optimismsepolia",
   "168587773": "blasttestnet",
