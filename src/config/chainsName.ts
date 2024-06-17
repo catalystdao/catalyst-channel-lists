f38e635e632a982b4dd29f09c9b90b0ab9daf2d3
Alexander <git@lindgren.xyz>
2024-06-17 10:44:42 +0200

    feat: add LZ with sei and xai

--- src/config/chainsName.ts
+++ src/config/chainsName.ts
@@ -8,18 +8,26 @@ export const chainsName: Record<string, string> = {
   "optimismsepolia": "11155420",
   "basesepolia": "84532",
   "blasttestnet": "168587773",
+  "seitestnet": "1328",
+  "xaitestnet": "37714555429",
   "optimism": "10",
   "base": "8453",
-  "blast": "81457"
+  "blast": "81457",
+  "sei": "1329",
+  "xai": "660279"
 };
 
 export const reverseChainsName: Record<string, string> = {
   "10": "optimism",
+  "1328": "seitestnet",
+  "1329": "sei",
   "8453": "base",
   "81457": "blast",
   "84532": "basesepolia",
   "421614": "arbitrumsepolia",
+  "660279": "xai",
   "11155111": "sepolia",
   "11155420": "optimismsepolia",
-  "168587773": "blasttestnet"
+  "168587773": "blasttestnet",
+  "37714555429": "xaitestnet"
 };
